b546ce5f6f71b8408e667c2a7c2e5a2b10bf10b6
Styx <Yusufibrahim117@hotmail.com>
2024-04-14 16:24:36 +0100

    add basic tests for xbl

---
+++ tests/Feature/XBLTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class XBLTest extends TestCase {
+    /**
+    * A basic feature test example.
+    *
+    * @return void
+    */
+
+    public function testXblUsernameEndpointReturnsCorrectJson() {
+        $response = $this->get( '/lookup?type=xbl&username=tebex' );
+
+        $response->assertStatus( 200 )
+        ->assertJson( json_decode( '{"username":"Tebex","id":"2533274844413377","avatar":"https:\/\/avatar-ssl.xboxlive.com\/avatar\/2533274844413377\/avatarpic-l.png"}', true ) );
+    }
+
+    public function testXblIdEndpointReturnsCorrectJson() {
+        $response = $this->get( '/lookup?type=xbl&id=2533274884045330' );
+
+        $response->assertStatus( 200 )
+        ->assertJson( json_decode( '{"username":"d34dmanwalkin","id":"2533274884045330","avatar":"https:\/\/avatar-ssl.xboxlive.com\/avatar\/2533274884045330\/avatarpic-l.png"}', true ) );
+    }
+}
