bfc1029a3f07d639f0a7f166606ae1df66383f39
Styx <Yusufibrahim117@hotmail.com>
2024-04-14 16:28:38 +0100

    add basic tests for minecraft

---
+++ tests/Feature/MinecraftTest.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Tests\Feature;
+
+use Tests\TestCase;
+
+class MinecraftTest extends TestCase {
+    /**
+    * A basic feature test example.
+    *
+    * @return void
+    */
+
+    public function testMinecraftUsernameEndpointReturnsCorrectJson() {
+        $response = $this->get( '/lookup?type=minecraft&username=Notch' );
+
+        $response->assertStatus( 200 )
+        ->assertJson( json_decode( '{"username":"Notch","id":"069a79f444e94726a5befca90e38aaf5","avatar":"https:\/\/crafatar.com\/avatars/069a79f444e94726a5befca90e38aaf5"}', true ) );
+    }
+
+    public function testMinecraftIdEndpointReturnsCorrectJson() {
+        $response = $this->get( '/lookup?type=minecraft&id=d8d5a9237b2043d8883b1150148d6955' );
+
+        $response->assertStatus( 200 )
+        ->assertJson( json_decode( '{"username":"Test","id":"d8d5a9237b2043d8883b1150148d6955","avatar":"https:\/\/crafatar.com\/avatars/d8d5a9237b2043d8883b1150148d6955"}', true ) );
+    }
+}
